c9b82a97344b43e3f446caff320a39691de9a01f
gilbert-sudo <mikamickswonder@gmail.com>
2022-03-25 20:37:11 +0300

    donation controller finished

--- libraries/models/Donation.php
+++ libraries/models/Donation.php
@@ -1,14 +1,14 @@
 <?php
- namespace models;
- require_once('../autoload.php');
- class Donation extends Model
- {    
-      public function ask(array $variable = [])
-      { 
-        extract($variable);
-        $insertAsking = $this->pdo->prepare("INSERT INTO demande_dons(id, but, font, description,images) VALUES (null, ?, ?, ?, ?)");  
-        $insertAsking -> execute(compact('but','font','description','images'));
- }   
- }
 
-?>
+namespace models;
+
+require_once('libraries/autoload.php');
+class Donation extends Model
+{
+  public function ask(array $variable = [])
+  {
+    extract($variable);
+    $insertAsking = $this->pdo->prepare("INSERT INTO demande_dons(id, but, font, description,images) VALUES (null, ?, ?, ?, ?)");
+    $insertAsking->execute(compact('but', 'font', 'description', 'images'));
+  }
+}
