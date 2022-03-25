88cc20d7cba8b8ce9aae06db8cc61a8c7c786589
randrianarschen <93827707+randrianarschen@users.noreply.github.com>
2022-03-25 20:19:54 +0300

    donation file changed

---
+++ libraries/models/Donation.php
@@ -0,0 +1,13 @@
+<?php
+ namespace models;
+ require_once('../autoload.php');
+ class Donation extends Model
+ {    
+      public function ask()
+      {
+        $insertAsking = $this->pdo->prepare("INSERT INTO demande_dons(id, but, font, description,images) VALUES (null, ?, ?, ?, ?)");  
+        $insertAsking -> execute(compact('but','font','description','images'));
+ }   
+ }
+
+?>
