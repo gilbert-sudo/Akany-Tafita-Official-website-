1bec1528344069dcc20b1d2df6b15dca6f658d5c
randrianarschen <93827707+randrianarschen@users.noreply.github.com>
2022-03-25 22:49:07 +0300

    behavior at controller donation created

--- libraries/controllers/Donation.php
+++ libraries/controllers/Donation.php
@@ -1,10 +1,29 @@
 <?php
 namespace Controllers;
-
+require_once('libraries/autoload.php');
 class Donation extends Controller
 {
     public function index()
-    {
-        \Renderer::render('donation/index');
+    {   
+      
+            \Renderer::render('donation/index');
+         
+      }
+      public function Ask(){
+         if(isset($_POST['publier'])) {
+            if(isset($_POST['but']) and isset($_POST['fond']) and isset($_POST['description']) and isset($_FILES['images'])){
+              $but = $_POST['but'];
+              $fond = $_POST['fond'];
+              $description = $_POST['description'];
+              $img_name = $_FILES['images']['name'];
+              $img_ext = explode('.',$img_name);
+              $imgActExt = strtolower(end($img_ext));
+              $images = uniqid('',true).".".$img_ext;
+              $asking = new \models\Donation();
+              $asking->Ask(array($but,$fond,$description,$images));
+              \Http::redirect('donation/index.html.php');
+
+      }
     }
+}
 }
--- libraries/models/Donation.php
+++ libraries/models/Donation.php
@@ -3,8 +3,9 @@
  require_once('../autoload.php');
  class Donation extends Model
  {    
-      public function ask()
-      {
+      public function ask(array $variable = [])
+      { 
+        extract($variable);
         $insertAsking = $this->pdo->prepare("INSERT INTO demande_dons(id, but, font, description,images) VALUES (null, ?, ?, ?, ?)");  
         $insertAsking -> execute(compact('but','font','description','images'));
  }   
