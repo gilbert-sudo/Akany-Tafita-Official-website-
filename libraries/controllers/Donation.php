d7d934341340635bc558873e21c630deb375a0d7
randrianarschen <93827707+randrianarschen@users.noreply.github.com>
2022-03-26 09:47:18 +0300

    \controllers\Donation updated

--- libraries/controllers/Donation.php
+++ libraries/controllers/Donation.php
@@ -1,29 +1,39 @@
 <?php
+
 namespace Controllers;
+
 require_once('libraries/autoload.php');
 class Donation extends Controller
 {
-    public function index()
-    {   
-      
-            \Renderer::render('donation/index');
-         
-      }
-      public function Ask(){
-         if(isset($_POST['publier'])) {
-            if(isset($_POST['but']) and isset($_POST['fond']) and isset($_POST['description']) and isset($_FILES['images'])){
-              $but = $_POST['but'];
-              $fond = $_POST['fond'];
-              $description = $_POST['description'];
-              $img_name = $_FILES['images']['name'];
-              $img_ext = explode('.',$img_name);
-              $imgActExt = strtolower(end($img_ext));
-              $images = uniqid('',true).".".$img_ext;
-              $asking = new \models\Donation();
-              $asking->Ask(array($but,$fond,$description,$images));
-              \Http::redirect('donation/index.html.php');
+  public function index()
+  {
 
+    \Renderer::render('donation/index');
+  }
+  public function Ask()
+  {
+    if (isset($_POST['publier'])) {
+      if (isset($_POST['but']) and isset($_POST['fond']) and isset($_POST['description']) and isset($_FILES['images'])) {
+        $but = $_POST['but'];
+        $fond = $_POST['fond'];
+        $description = $_POST['description'];
+        $img_name = $_FILES['images']['name'];
+        $target_tmp = $_FILES['images']['tmp_name'];
+        $img_ext = explode('.', $img_name);
+        $img_error = $_FILES['images']['error'];
+        $allowed = array('jpg', 'jpeg', 'png');
+        $imgActExt = strtolower(end($img_ext));
+        if ($img_error === 0) {
+          if (in_array($imgActExt, $allowed)) {
+            $images = uniqid('', true) . "." . $imgActExt;
+            $img_dest = 'views/images' . $images;
+            move_uploaded_file($target_tmp, $img_dest);
+            $asking = new \models\Donation();
+            $asking->Ask(array($but, $fond, $description, $images));
+            \Http::redirect('donation/index.html.php');
+          }
+        }
       }
     }
-}
+  }
 }
