41dfc82f403d34c04ce7eb9f1d6854c1f628081a
randrianarschen <93827707+randrianarschen@users.noreply.github.com>
2022-04-23 16:44:56 +0300

    I fixed the manage event frontend

--- views/javascript/event.js
+++ views/javascript/event.js
@@ -27,12 +27,11 @@ function save_row(no)
  var timeNew=document.getElementById("edit_time"+no).value;
  var descNew=document.getElementById("edit_desc"+no).value;
  var srcNew = document.getElementById("blah").src;
- 
  document.getElementById("title_row"+no).innerHTML=titleNew;
  document.getElementById("date_row"+no).innerHTML=dateNew;
  document.getElementById("time_row"+no).innerHTML=timeNew;
  document.getElementById("desc_row"+no).innerHTML=descNew;
- document.getElementById("img_row"+no).innerHTML= "<img id='event_img1' src='"+srcNew+"' width='100px' height='100px'>"
+ document.getElementById("img_row"+no).innerHTML= "<img id='event_img"+no+"' src='"+srcNew+"' width='100px' height='100px'>"
  document.getElementById("edit_button"+no).style.display="block";
  document.getElementById("save_button"+no).style.display="none";
 } 
@@ -51,13 +50,13 @@ function add_row()
  var newImgSrc= document.getElementById('blah').src;
  var table=document.getElementById("data_table");
  var table_len=(table.rows.length)-1;
- var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'><td id='title_row"+table_len+"'>"+newTitle+"</td><td id='date_row"+table_len+"'>"+newDate+"</td><td id='time_row"+table_len+"'>"+newTime+"</td><td id='desc_row"+table_len+"'>"+newDesc+"</td><td id='img_row"+table_len+"'><img src='"+newImgSrc+"' id='event_img"+table_len+"' width='100px' height='100px'></td><td ><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'><input type='button' id='save_button"+table_len+"'  value='Save' class='save' style='display:none;' onclick='save_row("+table_len+")'> <input type='button' value='Delete' class='delete' onclick='delete_row('"+table_len+"')'></td></tr>";
+ var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'><td id='title_row"+table_len+"'>"+newTitle+"</td><td id='date_row"+table_len+"'>"+newDate+"</td><td id='time_row"+table_len+"'>"+newTime+"</td><td id='desc_row"+table_len+"'>"+newDesc+"</td><td id='img_row"+table_len+"'><img src='"+newImgSrc+"' id='event_img"+table_len+"' width='100px' height='100px'></td><td ><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'><input type='button' id='save_button"+table_len+"'  value='Save' class='save' style='display:none;' onclick='save_row("+table_len+")'><input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
 newTitle = " " ;
 newDate =" " ;
 newTime = " ";
 newDesc = " ";
 newImgSrc = " #";
-}
+}                                                        
  function showPreview(event){
     if(event.target.files.length > 0){
       var src = URL.createObjectURL(event.target.files[0]);
